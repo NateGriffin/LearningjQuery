2608c1fcd712928a08993174e597da683ad6d967
Nate Griffin <nategriffin1@gmail.com>
2014-02-10 22:53:26 -0500

    Continuing Lesson 4 with templating

--- Lesson3/slider/slider-refractored.js
+++ Lesson3/slider/slider-refractored.js
@@ -1,29 +1,37 @@
-(function($){
+(function($) {
 
     //function function_name = Object
 
-   function SliderObj(container,nav) {
+    var SliderObj = function(container, nav) {
         this.container = container;
         this.nav = nav;
         this.imgs = this.container.find('img'); //find all imgs within contaier(slider div)
         this.imgsWidth = this.imgs[0].width; //get the width of the first image
         this.imgsLength = this.imgs.length; //get the number of images
         this.current = 0;
-   }
+    };
 
-   SliderObj.prototype.transition = function(coords) {
+    SliderObj.prototype.transition = function(coords) {
         this.container.animate({
-            'margin-left':coords || -(this.current * this.imgsWidth)
+            'margin-left': coords || -(this.current * this.imgsWidth)
         });
-            
-   };
 
-   //var slider = new Slider($('div.slider'), $('#slider-nav'));
-   var slider = new SliderObj($('div.slider'), $('#slider-nav'));
+    };
 
-        slider.nav.show().find('button').on('click', function() {
-            slider.transition(-1800);
 
-        });
+    SliderObj.prototype.setCurrent = function(dir) {
+        (dir === 'next') ? this.current++ : this.current--;
+        this.current = (this.current < 0) ? this.imgsLength - 1 : this.current % this.imgsLength;
+        return this.current;
+    };
+
+    //var slider = new Slider($('div.slider'), $('#slider-nav'));
+    var slider = new SliderObj($('div.slider-container'), $('#slider-nav'));
+
+    slider.nav.show().find('button').on('click', function() {
+        slider.setCurrent($(this).data('dir'));
+        slider.transition();
+
+    });
 
 })(jQuery);
